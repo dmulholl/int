ae5e750a2cbaea56edb7548801e90632066e525f
Darren Mulholland <dmulholland@outlook.ie>
2014-10-01 16:44:42 +0100

    Print binary representation in octet form

--- int.py
+++ int.py
@@ -12,16 +12,16 @@ Example:
 
     $ int x10
     
-    bin: 10000
-    oct: 20
-    dec: 16
     hex: 10
+    dec: 16
+    oct: 20
+    bin: 00010000
 
 License: This work has been placed in the public domain.
 
 """
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 
 import os, sys
@@ -29,7 +29,7 @@ import os, sys
 
 help_text = """usage: %s INT [INT ...]
 
-Print an integer in its [b]inary, [o]ctal, [d]ecimal, and he[x] bases.
+Prints an integer in its [b]inary, [o]ctal, [d]ecimal, and he[x] bases.
 
 Use a single letter prefix to declare the base of the input, e.g. b1010. 
 The base defaults to decimal if no prefix is supplied. Multiple arguments 
@@ -37,10 +37,10 @@ are accepted.""" % os.path.basename(__file__)
 
 
 template = """
-bin: {0:b}
+hex: {0:X}
+dec: {0:,d}
 oct: {0:o}
-dec: {0:d}
-hex: {0:X}\
+bin: \
 """
 
 
@@ -49,27 +49,49 @@ bases = {
     'o': (8, 'an octal'),
     'd': (10, 'a decimal'),
     'x': (16, 'a hexadecimal'),
+    'h': (16, 'a hexadecimal'),
 }
+
+
+def binary_string(i):
+    s = ''
+    while i:
+        for digit in range(8):
+            if i & 1 == 1:
+                s = '1' + s
+            else:
+                s = '0' + s
+            i >>= 1
+        s = ' ' + s
+    return s.lstrip()
 
 
 def print_ints(args):
     for argstr in args:
         prefix = argstr[0].lower()
         if prefix in bases:
-            intstr = argstr[1:]
+            inp_str = argstr[1:]
             base, adj = bases[prefix]
         else:
-            intstr = argstr
+            inp_str = argstr
             base, adj = bases['d']
         try:
-            print(template.format(int(intstr, base)))
+            inp_int = int(inp_str, base)
         except ValueError:
-            print("\nerror: [%s] cannot be parsed as %s integer" % (intstr, adj))
+            sys.exit(
+                '\nerror: "%s" cannot be parsed as %s integer' % (inp_str, adj)
+            )
+        print(template.format(inp_int) + binary_string(inp_int))
 
 
-if __name__ == '__main__':
-    if len(sys.argv) == 1 or sys.argv[1] in ('-h', '--help'):
+def main():
+    if len(sys.argv) == 1:
+        print(help_text)
+    elif '-h' in sys.argv or '--help' in sys.argv:
         print(help_text)
     else:
         print_ints(sys.argv[1:])
 
+
+if __name__ == '__main__':
+    main()
